d33944519f51feed4d3f5dda5c295c1514364bec
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2023-04-09 23:09:43 -0400

    add daily convenience function, update readme

--- atomic_counter/__init__.py
+++ atomic_counter/__init__.py
@@ -1,3 +1,23 @@
 __version__ = "0.1.0"
 
-from .atomic_counter import *
+from functools import lru_cache
+from datetime import datetime
+from dateutil import tz
+
+from .atomic_counter import Counter
+
+
+def daily() -> Counter:
+    '''Generate a counter which is guaranteed to produce unique numbers
+    for a single day, regardless of instantiation time, up to a granularity
+    of 1ns'''
+
+    nowish = datetime.utcnow()
+    base = datetime(nowish.year, nowish.month, nowish.day, tzinfo=tz.UTC)
+
+    return Counter(0, int(base.timestamp()) * 1_000_000_000)
+
+__all__ = [
+    "Counter",
+    "daily",
+]
---
+++ atomic_counter/tests/test_daily.py
@@ -0,0 +1,18 @@
+from atomic_counter import daily
+from datetime import datetime
+
+
+class TestOffset:
+    def test_bounds(self):
+        counter = daily()
+        nowish = datetime.utcnow()
+        seconds_today = nowish.second + nowish.minute * 60 + nowish.hour * 3600
+
+        # offset for small delay
+        seconds_today += 1
+
+        # convert to nanos
+        nanos_today = seconds_today * 1_000_000_000
+        assert 1_000_000_000 < counter.next() < nanos_today
+
+
